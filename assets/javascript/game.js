06f0db492c9bbb759812c5c7bde735c7e850b95c
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-06-25 20:27:48 -0500

    pseudocoded game

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// make an array containing movies
+
+
+//make computer choose a random movie from array
+
+
+//turn word into underscore do this by using string.length and then of those letters add same amount of underscores use a loop
+
+
+//have user pick a letter onkey up event use append to replace underscore with letter
+
+
+//if letter is in word reveal letter replace underscore
+
+
+//if letter is not lose a life
+
+
+//if all letters revealed win display movie and play song
+
+
+//reset and restart the game
