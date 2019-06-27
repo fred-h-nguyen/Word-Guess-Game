ea9066365319422a6dcec24a48a6a3ea0a1d6c27
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-06-27 14:49:05 -0500

    removed alphabet variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 //variables go here
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var word = [];
 var correct = [];
 var spaces = 0;
